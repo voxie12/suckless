5d30f681b2b501ead517ac5507a0edb12ba7edac
jay <bharadia2@gmail.com>
2021-07-26 01:18:04 +0100

    added slock and surf

--- slstatus/config.h
+++ slstatus/config.h
@@ -66,7 +66,9 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function     format          argument */
 	{ run_command,  " 摒 %s | ",    "curl -s 'wttr.in?format=%C+%t'" },
-	{ netspeed_rx,  " %sB/s | ",   "enp4s0" },
+	{ netspeed_rx,  " %sB/s ",   "enp4s0" },
+	{ disk_used,  "[ %s / ",   "/" },
+	{ disk_total,  " %s ] ",   "/" },
 	/*{ netspeed_rx, " %sB/s | ",   "wlp3s0" },*/
 	/*{ wifi_essid,   " %s ",      "wlp3s0" },*/
 	/*{ wifi_perc,    "%s | ",      "wlp3s0" },*/
